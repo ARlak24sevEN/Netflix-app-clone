4aea2d0e51450c0b92ceef107b226a51a651fc23
Arlak <arlak.a@orcsoft.co.th>
2023-04-10 15:03:48 +0700

    Initial Commit

---
+++ Netflix-app-clone/Netflix_app_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_app_cloneApp.swift
+//  Netflix-app-clone
+//
+//  Created by Arlak Abdulloh on 10/4/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_app_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
